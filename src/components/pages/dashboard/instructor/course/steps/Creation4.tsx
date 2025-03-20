c0a04f8457b1936807dc9d3185a97a1f6fc698b5
Showkat  Ali <showkat.ali2634@gmail.com>
2025-03-20 12:41:26 +0600

    adding

--- src/components/pages/dashboard/instructor/course/steps/Creation4.tsx
+++ src/components/pages/dashboard/instructor/course/steps/Creation4.tsx
@@ -104,11 +104,129 @@ const Creation4 = (props: StepPropss) => {
     }
   }, [isError, isSuccess]);
   return (
+    // <>
+    //   <FaqsCreateModal
+    //     show={moduleModalShow}
+    //     setShowModal={setmoduleModalShow}
+    //   />
+    //   <form onSubmit={handleSubmit(courseRegister)}>
+    //     <div className="p-3 mt-5">
+    //       <h2 className="font-semibold text-2xl mb-5">
+    //         Additional Information
+    //       </h2>
+    //       <div>
+    //         <div className="heading_upload_lectrue flex justify-between items-center mb-4">
+    //           <h2 className="text-base font-medium">Upload Faqs</h2>
+    //           <button
+    //             type="button"
+    //             onClick={() => setmoduleModalShow(!moduleModalShow)}
+    //             className="bg-[#EBEEFD] border border-[#3A57E8] px-3 py-2 rounded flex items-center gap-[6px]"
+    //           >
+    //             <Image src={plusIconBg} alt="" />
+    //             Add Question
+    //           </button>
+    //         </div>
+    //         {/* {faqsIsSuccess &&
+    //           faqsData?.data?.faqs?.map((item: any) => (
+    //             <div
+    //               className="p-4 mb-4"
+    //               style={{ boxShadow: "0px 1px 15px rgba(0, 0, 0, 0.15)" }}
+    //               key={item}
+    //             >
+    //               <div className="flex justify-between items-center">
+    //                 <h2 className="text-base font-medium">{item.question}?</h2>
+    //                 <div className="flex justify-between gap-2">
+    //                   <div className="bg-[#D5EBDF] rounded-full w-[32px] h-[32px] flex justify-center items-center">
+    //                     <Image src={editIcon} width={18} height={16} alt="" />
+    //                   </div>
+    //                   <div className="bg-[#F2D6D3] rounded-full w-[32px] h-[32px] flex justify-center items-center">
+    //                     <Image src={closeIcon} width={14} height={13} alt="" />
+    //                   </div>
+    //                 </div>
+    //               </div>
+    //               <div className="mt-2">
+    //                 <p className="text-[#8A92A6]">{item.answer}</p>
+    //               </div>
+    //             </div>
+    //           ))} */}
+
+    //         <div className="tag_url flex flex-col mb-5">
+    //           <label className="text-base font-medium mb-3">Tags</label>
+    //           <Controller
+    //             control={control}
+    //             name="tags"
+    //             rules={{ required: "tags is required" }}
+    //             render={({ field }) => (
+    //               <TagsInput
+    //                 {...field}
+    //                 value={formData.tags}
+    //                 onChange={(text: any) => {
+    //                   field.onChange(text);
+    //                 }}
+    //               />
+    //             )}
+    //           />
+    //           {errors.tags && <InputErrorMessage message={"Enter your tag"} />}
+    //         </div>
+    //         <div
+    //           className="upload_image_details flex justify-between bg-[#CDEBEC] mt-10 mb-8 flex-col sm:flex-row items-center"
+    //           style={{ padding: "12px 21px" }}
+    //         >
+    //           <div className="flex items-center gap-2">
+    //             <span className="text-[13px] sm:text-[10px] lg:text-base font-normal text-[#056C71]">
+    //               Max keywords: 14
+    //             </span>
+    //           </div>
+    //           <div className="flex items-center gap-2">
+    //             <span className="text-[13px] sm:text-[10px] lg:text-base text-[#056C71]">
+    //               in lowercase and separated by commas
+    //             </span>
+    //           </div>
+    //           <div className="flex items-center gap-2">
+    //             <span className="text-[13px] sm:text-[10px] lg:text-base text-black">
+    //               e.g. javascript, react, marketing
+    //             </span>
+    //           </div>
+    //         </div>
+
+    //         <div className="message flex flex-col mb-3">
+    //           <label className="text-base font-medium mb-3">
+    //             Message to a reviewer
+    //           </label>
+    //           <textarea
+    //             {...register("msgtoreviewer", { required: true })}
+    //             placeholder="Write a message"
+    //             className="p-3 border-none"
+    //             style={{ boxShadow: "0px 1px 15px rgba(0, 0, 0, 0.15)" }}
+    //           ></textarea>
+    //           {errors.msgtoreviewer && (
+    //             <InputErrorMessage message={"Enter message reviewer"} />
+    //           )}
+    //         </div>
+
+    //         <div className="btn flex justify-end gap-5 mt-8">
+    //           <button
+    //             className="xsm:w-full lg:w-[8rem] bg-[#EBEEFD] border border-[#3A57E8] py-2 px-4 rounded"
+    //             onClick={onPrev}
+    //           >
+    //             Previous
+    //           </button>
+    //           <button
+    //             className="flex xsm:w-full lg:w-[9rem] justify-center bg-[#3A57E8] py-2 px-4 text-[#fff] rounded"
+    //             type="submit"
+    //           >
+    //             {isLoading ? <ButtonLoader /> : "Submit Course"}
+    //           </button>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </form>
+    // </>
     <>
       <div>
         <div className="">
           <div className="flex justify-center">
-            {/* <Image src={PinRightIcon} width={540} height={390} alt="" /> */}
+            {/* <Image src={RightIcon} width={540} height={390} alt="" /> */}
           </div>
           <div className="text-center  w-full m-auto mb-56">
             <h2 className="font-semibold text-[1.4rem]">
